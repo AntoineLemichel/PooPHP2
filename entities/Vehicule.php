5edf821e2b469659b325d42c11aa38ff6515fce4
antoineLemichel <antoine.lemichel@newgates.fr>
2018-11-28 10:11:34 +0100

    Adding class parent vehicule

---
+++ entities/Vehicule.php
@@ -0,0 +1,174 @@
+<?php
+
+declare(strict_types=1);
+class Vehicule
+{
+    protected $motors;
+    protected $type;
+    protected $door;
+    protected $brand;
+    protected $fuel;
+
+    public function __construct(array $array)
+    {
+        $this->hydrate($array);
+    }
+
+    public function hydrate(array $array)
+    {
+        foreach ($donnees as $key => $value) {
+            // On récupère le nom du setter correspondant à l'attribut.
+            $method = 'set'.ucfirst($key);
+
+            // Si le setter correspondant existe.
+            if (method_exists($this, $method)) {
+                // On appelle le setter.
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * GETTERS.
+     */
+
+    /**
+     * Getters for motors.
+     *
+     * @return self
+     */
+    public function getMotors()
+    {
+        return $this->motors;
+    }
+
+    /**
+     * Getters for type of vehicuele.
+     *
+     * @return self
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Getters for door on a vehicule.
+     *
+     * @return self
+     */
+    public function getDoor()
+    {
+        return $this->door;
+    }
+
+    /**
+     * Getters for vehicule's brand.
+     *
+     * @return self
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Getters for vehicule's fuel.
+     *
+     * @return self
+     */
+    public function getFuel()
+    {
+        return $this->fuel;
+    }
+
+    /*
+     * SETTERS
+     */
+
+    /**
+     * Setters for motor.
+     *
+     * @param string $motors
+     *
+     * @return self
+     */
+    public function setMotors(string $motors)
+    {
+        $motors = (string) $motors;
+        if (is_string($motors)) {
+            $this->motors = $motors;
+
+            return $this;
+        }
+    }
+
+    /**
+     * Setters for type.
+     *
+     * @param string $type
+     *
+     * @return self
+     */
+    public function setType(string $type)
+    {
+        $type = (string) $type;
+        if (is_string($type)) {
+            $this->type = $type;
+
+            return $this;
+        }
+    }
+
+    /**
+     * Setters for doors.
+     *
+     * @param int $door
+     *
+     * @return self
+     */
+    public function setDoor(int $door)
+    {
+        $door = (int) $door;
+        if (is_int($door)) {
+            $this->door = $door;
+
+            return $this;
+        }
+    }
+
+    /**
+     * Setters for vehicule's brand.
+     *
+     * @param string $brand
+     *
+     * @return self
+     */
+    public function setBrand(string $brand)
+    {
+        $brand = (string) $brand;
+        if (is_string($brand)) {
+            $this->brand = $brand;
+
+            return $brand;
+        }
+    }
+
+    /**
+     * Setters for vehicule's fuel.
+     *
+     * @param string $fuel
+     *
+     * @return self
+     */
+    public function setFuel(string $fuel)
+    {
+        $fuel = (string) $fuel;
+
+        if (is_string($fuel)) {
+            $this->fuel = $fuel;
+
+            return $this;
+        }
+    }
+}
