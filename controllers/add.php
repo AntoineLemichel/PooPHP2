ed78bd40f58fd7c2dc6fb40afd69e0ca883b9ca8
antoineLemichel <antoine.lemichel@newgates.fr>
2018-12-04 11:02:42 +0100

    Adding verif

--- controllers/add.php
+++ controllers/add.php
@@ -14,37 +14,50 @@ $db = Database::DB();
 $vehiculeManager = new VehiculeManager($db);
 
 if (isset($_POST['add'])) {
-    if ($_POST['type'] == 'Car') {
-        $opponentCar = new Car([
-            'brand' => $_POST['brand'],
-            'motors' => $_POST['motors'],
-            'door' => $_POST['doors'],
-            'fuel' => $_POST['fuel'],
-            'type' => 'Car',
-        ]);
-        $vehiculeManager->addCar($opponentCar);
-        header('Location: index.php');
-    } elseif ($_POST['type'] == 'MotorBike') {
-        $opponentMotorBike = new MotorBike([
-            'brand' => $_POST['brand'],
-            'motors' => $_POST['motors'],
-            'fuel' => $_POST['fuel'],
-            'type' => 'MotorBike',
-        ]);
-        $vehiculeManager->addMotorBike($opponentMotorBike);
-        header('Location: index.php');
-    } elseif ($_POST['type'] == 'Truck') {
-        $opponentTruck = new Truck([
-            'brand' => $_POST['brand'],
-            'motors' => $_POST['motors'],
-            'door' => $_POST['doors'],
-            'fuel' => $_POST['fuel'],
-            'type' => 'Truck',
-        ]);
-        $vehiculeManager->addTruck($opponentTruck);
-        header('Location: index.php');
-    } else {
-        $message = 'The type is undefined.';
+    $brand = htmlspecialchars($_POST['brand']);
+    $motors = htmlspecialchars($_POST['motors']);
+    $doors = htmlspecialchars($_POST['doors']);
+    $fuel = htmlspecialchars($_POST['fuel']);
+
+    $doors = (int) $doors;
+    $brand = (string) $brand;
+    $motors = (string) $motors;
+    $fuel = (string) $fuel;
+    if (is_int($doors)) {
+        if ($doors >= 2 and $doors <= 5 or $doors == 0) {
+            if ($_POST['type'] == 'Car') {
+                $opponentCar = new Car([
+                    'brand' => $brand,
+                    'motors' => $motors,
+                    'door' => $doors,
+                    'fuel' => $fuel,
+                    'type' => 'Car',
+                ]);
+                $vehiculeManager->addCar($opponentCar);
+                header('Location: index.php');
+            } elseif ($_POST['type'] == 'MotorBike') {
+                $opponentMotorBike = new MotorBike([
+                    'brand' => $brand,
+                    'motors' => $motors,
+                    'fuel' => $fuel,
+                    'type' => 'MotorBike',
+                ]);
+                $vehiculeManager->addMotorBike($opponentMotorBike);
+                header('Location: index.php');
+            } elseif ($_POST['type'] == 'Truck') {
+                $opponentTruck = new Truck([
+                    'brand' => $brand,
+                    'motors' => $motors,
+                    'door' => $doors,
+                    'fuel' => $fuel,
+                    'type' => 'Truck',
+                ]);
+                $vehiculeManager->addTruck($opponentTruck);
+                header('Location: index.php');
+            } else {
+                $message = 'The type is undefined.';
+            }
+        }
     }
 }
 include '../views/addVue.php';
--- controllers/detail.php
+++ controllers/detail.php
@@ -18,46 +18,69 @@ if (isset($_GET['index']) and !empty($_GET['index'])) {
     $updateVehicule = $vehiculeManager->getVehicule($id);
 }
 if (isset($_POST['update'])) {
-    $id = $_GET['index'];
-    if (isset($_POST['motors']) and !empty($_POST['motors'])) {
-        if (isset($_POST['brand']) and !empty($_POST['brand'])) {
-            if ($_POST['type'] == 'Car') {
-                if (isset($_POST['doors']) and !empty($_POST['doors'])) {
-                    $opponentCar = $vehiculeManager->getVehicule($id);
-                    $opponentCar->hydrate(array(
-                        'brand' => $_POST['brand'],
-                        'motors' => $_POST['motors'],
-                        'door' => $_POST['doors'],
-                        'fuel' => $_POST['fuel'],
-                    ));
+    $brand = htmlspecialchars($_POST['brand']);
+    $motors = htmlspecialchars($_POST['motors']);
+    $doors = htmlspecialchars($_POST['doors']);
+    $fuel = htmlspecialchars($_POST['fuel']);
+
+    $doors = (int) $doors;
+    $brand = (string) $brand;
+    $motors = (string) $motors;
+    $fuel = (string) $fuel;
+
+    if (is_int($doors)) {
+        if ($doors >= 2 and $doors <= 5) {
+            $id = (int) $_GET['index'];
+            if (isset($_POST['motors']) and !empty($_POST['motors'])) {
+                if (isset($_POST['brand']) and !empty($_POST['brand'])) {
+                    if ($_POST['type'] == 'Car') {
+                        if (isset($_POST['doors']) and !empty($_POST['doors'])) {
+                            $opponentCar = $vehiculeManager->getVehicule($id);
+
+                            $opponentCar->hydrate(array(
+                                'brand' => $brand,
+                                'motors' => $motors,
+                                'door' => $doors,
+                                'fuel' => $fuel,
+                            ));
+                        } else {
+                            $message = "Door's field will not must be empty.";
+                        }
+                        $vehiculeManager->updateCar($opponentCar);
+                    } elseif ($_POST['type'] == 'MotorBike') {
+                        $opponentMotorBike = $vehiculeManager->getVehicule($id);
+                        $opponentMotorBike->hydrate(array(
+                                'brand' => $brand,
+                                'motors' => $motors,
+                                'fuel' => $fuel,
+                            ));
+                        $vehiculeManager->updateMotorBike($opponentMotorBike);
+                    } elseif ($_POST['type'] == 'Truck') {
+                        if (isset($_POST['doors']) and !empty($_POST['doors'])) {
+                            $opponentTruck = $vehiculeManager->getVehicule($id);
+                            $opponentTruck->hydrate(array(
+                            'brand' => $brand,
+                            'motors' => $motors,
+                            'door' => $doors,
+                            'fuel' => $fuel,
+                        ));
+                            $vehiculeManager->updateTruck($opponentTruck);
+                        } else {
+                            $message = "Door's field will not must be empty.";
+                        }
+                    }
+                    header('Location: index.php');
                 } else {
-                    $message = "Door's field will not must be empty.";
+                    $message = "Brand's field will not must be empty.";
                 }
-                $vehiculeManager->updateCar($opponentCar);
-            } elseif ($_POST['type'] == 'MotorBike') {
-                $opponentMotorBike = $vehiculeManager->getVehicule($id);
-                $opponentMotorBike->hydrate(array(
-                        'brand' => $_POST['brand'],
-                        'motors' => $_POST['motors'],
-                        'fuel' => $_POST['fuel'],
-                    ));
-                $vehiculeManager->updateMotorBike($opponentMotorBike);
-            } elseif ($_POST['type'] == 'Truck') {
-                $opponentTruck = $vehiculeManager->getVehicule($id);
-                $opponentTruck->hydrate(array(
-                    'brand' => $_POST['brand'],
-                    'motors' => $_POST['motors'],
-                    'door' => $_POST['doors'],
-                    'fuel' => $_POST['fuel'],
-                ));
-                $vehiculeManager->updateTruck($opponentTruck);
+            } else {
+                $message = "Motor's field will not must be empty.";
             }
-            header('Location: index.php');
         } else {
-            $message = "Brand's field will not must be empty.";
+            $message = 'There are far too many doors.';
         }
     } else {
-        $message = "Motor's field will not must be empty.";
+        $message = 'Door will must be a number.';
     }
 }
 include '../views/detailVue.php';
